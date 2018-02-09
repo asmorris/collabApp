6b487c0778e58ea784d879f166a3cb60c51ac998
Andrew Morrison <morro141@gmail.com>
2018-02-08 23:47:53 -0500

    Set up rspec for testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,14 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'capybara/poltergeist'
-require 'factory_bot'
-require 'capybara/rspec'
-
-config.include Devise::Test::IntegrationHelpers, type: :feature
-config.include FactoryBot::Syntax::Methods
-Capybara.javascript_driver = :poltergeist
-Capybara.server = :puma
-
 
 
 ENV['RAILS_ENV'] ||= 'test'
@@ -16,8 +7,11 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
 
+# Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara/poltergeist'
+require 'factory_bot'
+require 'capybara/rspec'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -65,6 +59,11 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end 
+
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include FactoryBot::Syntax::Methods
+  Capybara.javascript_driver = :poltergeist
+  Capybara.server = :puma
 
 
   # RSpec Rails can automatically mix in different behaviours to your tests
